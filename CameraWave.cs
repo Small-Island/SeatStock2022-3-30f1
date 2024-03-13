becc70c12246087ad418fadbb5eedf297e1aef32
Small-Island <ojima.yukiya@gmail.com>
2024-03-13 13:43:32 +0900

    update

--- CameraWave.cs
+++ CameraWave.cs
@@ -2,14 +2,17 @@ public class CameraWave : UnityEngine.MonoBehaviour
 {
     public UnityEngine.Transform centerEye;
     public UnityEngine.Transform room;
-    public float offset = 0;
+    [UnityEngine.SerializeField, UnityEngine.Range(1, 100)]
+    public int scale = 10;
+    [UnityEngine.SerializeField, UnityEngine.Range(1.6f, 2.0f)]
+    public float height = 1.6f;
     void Start() {
-
     }
     void Update() {
+        this.room.localScale = new UnityEngine.Vector3(this.scale, this.scale, this.scale);
         this.room.position = new UnityEngine.Vector3(
             this.centerEye.position.x,
-            this.centerEye.position.y - offset,
+            this.centerEye.position.y - height,
             this.centerEye.position.z
         );
     }
